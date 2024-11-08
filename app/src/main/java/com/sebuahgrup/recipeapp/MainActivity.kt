bd4cf6a2f12836d4db144958e90fb8e84dc2c9c1
apikapikk <144014251+apikapikk@users.noreply.github.com>
2024-11-08 18:57:37 +0700

    up

--- app/src/main/java/com/sebuahgrup/recipeapp/MainActivity.kt
+++ app/src/main/java/com/sebuahgrup/recipeapp/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.sebuahgrup.recipeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var loginButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //initialize
+        loginButton = findViewById(R.id.login_button_login)
+
+
+        //action
+        loginButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
